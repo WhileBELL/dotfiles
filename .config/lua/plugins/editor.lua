752f151316c057883464e8759f5086948f4db238
WhileBELL <bellbelieve.work@gmail.com>
2024-07-15 21:44:10 +0700

    added in a nvim

---
+++ .config/lua/plugins/editor.lua
@@ -0,0 +1,52 @@
+return {
+  -- Hihglight colors
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = {},
+  },
+  {
+    "telescope.nvim",
+    priority = 1000,
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+        wrap_results = true,
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          n = {},
+        },
+      })
+      opts.pickers = {
+        diagnostics = {
+          theme = "ivy",
+          initial_mode = "normal",
+          layout_config = {
+            preview_cutoff = 9999,
+          },
+        },
+      }
+      opts.extensions = {
+        file_browser = {
+          theme = "dropdown",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+        },
+      }
+      telescope.setup(opts)
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("file_browser")
+    end,
+  },
+}
