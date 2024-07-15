6aebf5e5e2899715ff32b243a7332f81798c1e4d
WhileBELL <bellbelieve.work@gmail.com>
2024-07-15 21:52:02 +0700

    added in nvim/lua

---
+++ .config/nvim/lua/plugins/config.lua
@@ -0,0 +1,10 @@
+return {
+  -- visual multi
+  { "mg979/vim-visual-multi" },
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = true,
+  },
+}
---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,130 @@
+return {
+  -- messages, cmdline and the popupmenu
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  -- notify timeout
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
+
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- incline line
+  {
+    "b0o/incline.nvim",
+    dependencies = {},
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local helpers = require("incline.helpers")
+      require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          local buffer = {
+            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+            " ",
+            { filename, gui = modified and "bold,italic" or "bold" },
+            " ",
+            guibg = "#363944",
+          }
+          return buffer
+        end,
+      })
+    end,
+  },
+
+  -- logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+    ██╗    ██╗██╗  ██╗██╗██╗     ███████╗██████╗ ███████╗██╗     ██╗     
+    ██║    ██║██║  ██║██║██║     ██╔════╝██╔══██╗██╔════╝██║     ██║     
+    ██║ █╗ ██║███████║██║██║     █████╗  ██████╔╝█████╗  ██║     ██║     
+    ██║███╗██║██╔══██║██║██║     ██╔══╝  ██╔══██╗██╔══╝  ██║     ██║     
+    ╚███╔███╔╝██║  ██║██║███████╗███████╗██████╔╝███████╗███████╗███████╗
+     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═════╝ ╚══════╝╚══════╝╚══════╝
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
