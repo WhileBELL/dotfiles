0ad97872d39a60faf9b029cf12db334633f7d8a2
WhileBELL <bellbelieve.work@gmail.com>
2024-07-16 11:02:48 +0700

    test use stow

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,34 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			-- "catppuccin", "everforest"
-			colorscheme = "catppuccin",
-		},
-	},
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "auto",
-				transparent_background = true,
-			})
-		end,
-	},
-	{
-		"neanias/everforest-nvim",
-		version = false,
-		lazy = false,
-		name = "everforest",
-		priority = 1000,
-		config = function()
-			require("everforest").setup({
-				background = "hard",
-				transparent_background_level = 2,
-			})
-		end,
-	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- "catppuccin", "everforest"
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "auto",
+        transparent_background = true,
+      })
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    name = "everforest",
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 2,
+      })
+    end,
+  },
 }
