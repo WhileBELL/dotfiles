df6c2f7c6c101cdab99f2e3c2ff4e3d8eff93d60
WhileBELL <bellbelieve.work@gmail.com>
2024-07-21 14:20:39 +0700

    config lualine nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -44,6 +44,32 @@ return {
 		end,
 	},
 
+	-- lualine
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local line = require("lualine")
+			line.setup({
+				options = {
+					icons_enabled = true,
+					theme = "kanagawa",
+					component_separators = { right = "✧ .*" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+					lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+				},
+				extensions = {
+					"neo-tree",
+					"mason",
+					"lazy",
+				},
+			})
+		end,
+	},
+
 	-- notify timeout
 	{
 		"rcarriga/nvim-notify",
